5544012a8b6cf3fe1e66ea66b6a6fc592e38f803
samlbest <sambest86@gmail.com>
2023-11-02 19:35:54 -0500

    Add filter attribute to controllers

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ApiExceptionFilterAttribute>());
 builder.Services.AddEndpointsApiExplorer();
 
 
